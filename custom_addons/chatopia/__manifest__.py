b91a7c0f6e904c2a007cb6c0e1311116f9b73993
Subin Khang <subinkhang102@gmail.com>
2025-05-23 20:07:15 +0700

    Add custom_addons for exercise

--- custom_addons/chatopia/__manifest__.py
+++ custom_addons/chatopia/__manifest__.py
@@ -6,12 +6,18 @@
     'description': """
         Chatopia is an omnichannel, centralized chat system.
     """,
-    'depends': ['base'],
+    'depends': ['base', 'sale', 'crm'],
+    'assets': {
+        'web.assets_backend': [
+            '/chatopia/static/src/css/style.css',
+        ],
+    },
     'data': [
         'security/ir.model.access.csv',
         # 'data/chatopia_mock_data.xml',
         'views/chatopia_menu.xml',
-        'views/chatopia_views.xml'
+        'views/chatopia_views.xml',
+        'views/my_custom_report.xml'
     ],
     'installable': True,
     'application': True,
